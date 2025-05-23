ad819c2eccd9f9cb55cc96b4650487c19b903d61
vijay-nseg <vijay.dev@nseg.com.au>
2025-05-23 12:36:25 +0530

    fix: miner fixes

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -8,13 +8,12 @@ use Illuminate\Http\Request;
 
 class ActivityController extends Controller
 {
+    // insert activities in 5 random user  
     public function insertDummyData(Request $request)
     {
-        // Example: insert 5 random activities for random users with current timestamp
 
-        $users = User::all();
-
-        foreach ($users->random(10) as $user) {
+        $users = User::all(); 
+        foreach ($users->random(5) as $user) {
             Activity::create([
                 'user_id' => $user->id,
                 'performed_at' => now()->subDays(rand(0, 30))->setTime(rand(6, 20), rand(0, 59)), // Random past 30 days
@@ -22,8 +21,10 @@ class ActivityController extends Controller
             ]);
         }
         app(LeaderboardController::class)->recalculate(new Request(['filter' => 'day']));
-
-        return redirect()->route('leaderboard.index', ['filter' => 'day'])
+        app(LeaderboardController::class)->recalculate(new Request(['filter' => 'month']));
+        app(LeaderboardController::class)->recalculate(new Request(['filter' => 'year']));
+ 
+        return redirect()->route('leaderboard.index', ['filter' => 'year'])
             ->with('success', 'Dummy activity data inserted and leaderboard recalculated!');
     }
 }
